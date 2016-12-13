1e10671f2ed58cc635cb9064e99e829195b93b0d
Wilovent <william.klein@viacesi.fr>
2016-12-13 08:40:25 +0100

    ajout d'un client kodi + un serveur web qui tourne dans UWP

---
+++ KodiRpc/SocketWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+
+namespace KodiRpc
+{
+    public class SocketWrapper : ISocket
+    {
+        private readonly StreamSocket _streamSocket;
+
+        public SocketWrapper(StreamSocket streamSocket)
+        {
+            _streamSocket = streamSocket;
+        }
+
+        public void Dispose()
+        {
+            _streamSocket.Dispose();
+        }
+
+        public async Task ConnectAsync(string hostName, int port)
+        {
+            await _streamSocket.ConnectAsync(new HostName(hostName), port.ToString());
+        }
+
+        public Stream GetInputStream()
+        {
+            return _streamSocket.InputStream.AsStreamForRead();
+        }
+    }
+}
